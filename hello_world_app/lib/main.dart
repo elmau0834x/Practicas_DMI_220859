75a28856d8c215de00ccc01eaa861f6aafd4e8e8
Mauricio <elamu0834x@gmail.com>
2026-09-11 16:21:10 -0600

    Practica 02

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
 import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main(){
@@ -10,9 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreen()
+      theme: ThemeData(
+        colorSchemeSeed: const Color.fromARGB(255, 240, 236, 43),
+      ),
+      home: const CounterFunctionsScreen()
     );
   }
 
